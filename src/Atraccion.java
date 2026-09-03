c1660d1aa9a1f849410576515833b4e961125cdc
karolrg1122-git <karolrg1122@gmail.com>
2026-09-02 22:15:36 -0500

    se crea el Constructor 1 este permite crear una atracción solamente con su nombre

--- src/Atraccion.java
+++ src/Atraccion.java
@@ -1,3 +1,11 @@
 public class Atraccion {
+    private String nombre;
+    private int capacidad;
+    private double precio;
+    private boolean disponible;
+
+    public Atraccion(String nombre) {
+        this.nombre = nombre;
+    }
 
 }
